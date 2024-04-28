b22532731f262640eae3539e08fdeb8de06662a4
PieroM1 <u201923446@upc.edu.pe>
2024-04-27 21:59:36 -0500

    update: terrain list component added

--- src/main.js
+++ src/main.js
@@ -2,14 +2,22 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from "primevue/config";
+import 'primeflex/primeflex.css';
 import router from './router'
-import Card from "primevue/card";
 
+
+import Card from "primevue/card";
+import Toolbar from "primevue/toolbar";
+import Button from "primevue/button";
+import Avatar from "primevue/avatar";
+
+import 'primevue/resources/themes/bootstrap4-light-blue/theme.css';
 
 
 
 const app = createApp(App)
 
-app.use(router)
-app.mount('#app')
+app.use(PrimeVue, router, {ripple: true})
 app.component('pv-card', Card)
+app.mount('#app')
---
+++ src/terrains/components/terrain-list.component.vue
@@ -0,0 +1,39 @@
+<script >
+
+import {defineComponent} from "vue";
+import TerrainCard from "@/terrains/components/terrain-card.component.vue";
+import {getAllTerrains} from "@/terrains/services/terrain.api.service.js";
+
+export default defineComponent({
+
+  name: "terrain-list",
+  components: {TerrainCard},
+
+  data() {
+    return {
+      terrains: []
+    };
+  },
+  async mounted() {
+    try {
+      // Llama a la función para obtener los terrenos desde la API
+      this.terrains = await getAllTerrains();
+    } catch (error) {
+      console.error('Error al obtener los terrenos:', error);
+      // Maneja el error según tus necesidades (por ejemplo, muestra un mensaje de error al usuario)
+    }
+  }
+})
+</script>
+
+<template>
+
+  <div>
+    <terrain-card v-for="terrain in terrains" :key="terrain.id" :terrain="terrain" />
+  </div>
+
+</template>
+
+<style >
+
+</style>
