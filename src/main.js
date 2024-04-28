318b8e5632d2eb7fd636436f2f83cc04929c571e
PieroM1 <u201923446@upc.edu.pe>
2024-04-27 21:03:08 -0500

    feat: add dependencies

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Card from "primevue/card";
+
+
+
 
 const app = createApp(App)
 
 app.use(router)
-
 app.mount('#app')
+app.component('pv-card', Card)
