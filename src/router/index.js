df1d4e38cd2edc379a168477dff9fdf6e6ce6b73
Ventura Chancafe, Eduardo Renato <eduventura7777@gmail.com>
2024-05-22 10:26:50 -0500

    feat: fix redirection navbar button

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,7 @@ import {createRouter, createWebHistory} from "vue-router";
 import TerrainsComponent from "@/views/terrains.component.vue";
 import ResourcesComponent from "@/views/resources.component.vue";
 import MachinerysComponent from "@/views/machinerys.component.vue";
-import HomePage from "@/views/collaboration.component.vue";
-import EditUserPage from "@/collaboration/pages/edit-user.component.vue";
-import AddUserPage from "@/collaboration/pages/add-user.component.vue";
-import UserDetailsPage from "@/collaboration/pages/user-detail.component.vue";
-import TestView from "@/team-management/pages/test.view.vue";
+import CollaborationComponent from "@/views/collaboration.component.vue";
 
 
 const router = createRouter({
@@ -16,11 +12,7 @@ const router = createRouter({
         {path: "/terrains", component: TerrainsComponent},
         {path: "/resources", component: ResourcesComponent},
         {path: '/machinarys', component: MachinerysComponent},
-        { path: '/members', component: HomePage },
-        { path: '/member/add', component: AddUserPage },
-        { path: '/member/edit/:id', component: EditUserPage, props: true },
-        { path: '/member/:id', component: UserDetailsPage, props: true },
-        {path: '/test', component: TestView}
+        {path: '/collaboration', component: CollaborationComponent}
 
     ],
 });
