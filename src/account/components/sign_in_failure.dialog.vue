610fb5ca0445d80a44de56b7b44640b37cf2985e
SN-Marcelo <149278298+SN-Marcelo@users.noreply.github.com>
2024-05-22 20:35:03 -0500

    Feat: Added Authentication Components

---
+++ src/account/components/sign_in_failure.dialog.vue
@@ -0,0 +1,19 @@
+<script>
+export default{
+  name: "sign_in_failure.dialog",
+}
+</script>
+
+<template>
+  <pv-dialog modal header="Error" :style="{ width: '30rem' }">
+    <div style="text-align:center">
+      <span class="p-text-secondary block mb-5">
+        Incorrect Email or Password
+      </span>
+    </div>
+  </pv-dialog>
+</template>
+
+<style scoped>
+
+</style>
